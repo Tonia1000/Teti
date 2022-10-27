7da134d131d8bf15171a2b0c047adcb889fb0396
Tonia1000 <99226928+Tonia1000@users.noreply.github.com>
2022-10-27 19:36:17 -0400

    Add files via upload

--- Python-Alex/teti_Antonia/app.py
+++ Python-Alex/teti_Antonia/app.py
@@ -1,14 +1,15 @@
  #a partir do pacote flask import o Flask
-from flask import Flask, render_template
-from PIL import Image, requests
+#from matplotlib.pyplot import get
+#from requests import post, request
+from flask import Flask, render_template, request
 meu_app = Flask(__name__)
 
 
 @meu_app.route('/home')
 def home():
-    im = Image.open(requests.get(url, stream=True).raw)
-
     return render_template('home.html')
+
+# ----------------------------------------------------------------
 
 # @''.route indica a rota
 #def indica ao python o nome da função ou define coisas
@@ -17,22 +18,24 @@ def principal():
     #return '<h2>Integração de HTML e FLASK</h2>'
     return render_template('abobrinha.html')
 
-
+# ----------------------------------------------------------------
 @meu_app.route('/sobre')
 def sobre():
     return render_template('sobre.html')
 
+# ----------------------------------------------------------------
 @meu_app.route('/form')
 def form():
     lista_nomes=['Stela', 'Caio', 'Maurício']
     return render_template('form.html', lista_nomes)
 
-
+# ----------------------------------------------------------------
 @meu_app.route('/test')
 def test():
     listaNomes = {''}
     return render_template('test.html', listaNomes)
 
+# ----------------------------------------------------------------
 @meu_app.route('/atv4')
 def calc():
     n1 = 0
@@ -40,36 +43,45 @@ def calc():
     
     return render_template('atv4.html', n1, n2)
 
+# ----------------------------------------------------------------
 @meu_app.route('/atv5')
 def atv5():
     
     return render_template('atv5.html')
 
+# ----------------------------------------------------------------
 @meu_app.route('/ativ5')
 def ativ5():
     
     return render_template('ativ5.html')
 
+# ----------------------------------------------------------------
 @meu_app.route('/atv6')
 def atv6():
     
     return render_template('atv6.html')
 
+# ----------------------------------------------------------------
 @meu_app.route('/atv7')
 def atv7():
     
     return render_template('atv7.html')
 
+# ----------------------------------------------------------------
 @meu_app.route('/atv8')
 def atv8():
-
-#    cosd=cosseno de delta e send= seno de delta / delta= o angulo de rotação
-#    x' = (x)*cosd-(y)*send
-#    y'=(x)*send+(y)*cosd
     
     return render_template('atv8.html')
 
-# @meu_app.route('/atv9')
-# def atv9():
+# ----------------------------------------------------------------
+@meu_app.route('/api_teti', methods=['POST','GET'])
+def blabla():
+    token = 'ABC'
+    if(request.method=='GET'):
+        return render_template('blabla.html')        
+    if(request.method=='POST'):
+        token_for=request.form.get('token')
+        if token_for==token:
+            return render_template('blabla.html', msn='success')
+        return render_template('blabla.html', msn='fail')
     
-#     return render_template('atv9.html')
